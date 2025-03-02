a37a0bbcb799cac1fbf41e01fb9743e243fd905f
Bishnu Timalsina <bishnutimalsina@Mac.attlocal.net>
2025-03-02 17:08:29 -0600

    DB and service is up and running

--- leaderboard_service/go.mod
+++ leaderboard_service/go.mod
@@ -2,11 +2,10 @@ module leaderboard_service
 
 go 1.22.12
 
-toolchain go1.24.0
+// toolchain go1.24.0
 
 require (
 	github.com/99designs/gqlgen v0.17.66
-	github.com/sirupsen/logrus v1.9.3
 	github.com/vektah/gqlparser/v2 v2.5.22
 )
 
@@ -23,7 +22,6 @@ require (
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/mod v0.23.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/tools v0.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- leaderboard_service/server.go
+++ leaderboard_service/server.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"leaderboard_service/graph"
+	"log"
 	"net/http"
 	"os"
 
@@ -12,18 +11,12 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/sirupsen/logrus"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
 const defaultPort = "8080"
 
-var log = logrus.New()
-
 func main() {
-	log.SetFormatter(&logrus.JSONFormatter{})
-	log.SetLevel(logrus.InfoLevel)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -47,30 +40,4 @@ func main() {
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
-
-	// For now, directly connect to db
-	// Get database connection details from environment variables
-	dbHost := os.Getenv("DB_HOST")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-
-	// Construct the connection string
-	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
-		dbHost, dbUser, dbPassword, dbName)
-
-	// Connect to the database
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer db.Close()
-
-	// Test the connection
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Failed to ping database: %v", err)
-	}
-
-	fmt.Println("Successfully connected to the database")
 }
